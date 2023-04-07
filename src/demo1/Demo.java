3394ecbd8520ffa7e2ae95f8324008825a81d277
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-04-07 16:37:26 -0600

    Test code

---
+++ src/demo1/Demo.java
@@ -0,0 +1,19 @@
+package demo1;
+
+import java.util.Scanner;
+
+class Demo {
+	public static void main(String[] args) {
+
+		        Scanner scanner = new Scanner(System.in);
+		        int price = scanner.nextInt();
+		        //complete the code
+		        if (price <12000) {
+		            System.out.println("enough");
+		        
+		        }
+
+		    }
+}
+
+
